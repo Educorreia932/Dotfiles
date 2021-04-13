948abc0c883462f062aae4d1b0472f3497416e40
Educorreia932 <skelozard1@gmail.com>
2021-04-13 22:31:24 +0100

    Added e-mail and username Git configuration to setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Copy files
 
-echo "Do you want to copy the the configuration files and folders?"
+echo "Do you want to copy the the configuration files and folders? [Y/n]"
 read -n 1 -r; \
     if [[ $$REPLY =~ ^[Yy] ]]; \
     then \
@@ -12,3 +12,9 @@ read -n 1 -r; \
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -c PlugInstall
 sudo apt install nodejs
+
+# Git configuration
+
+git config --global user.email "skelozard1@gmail.com"
+git config --global user.name "Educorreia932"
+
