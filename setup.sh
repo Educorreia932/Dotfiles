027c50af05f5a07c5a64bd2ae09439c04c511461
Educorreia932 <skelozard1@gmail.com>
2021-04-15 14:41:27 +0100

    Detecting if Dotfiles folder exists before moving files Closes #5

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
+#!/bin/bash
+
 # Copy files
 
-echo "Do you want to copy the the configuration files and folders? [Y/n]"
-read -n 1 -r; \
-    if [[ $$REPLY =~ ^[Yy] ]]; \
-    then \
-        rsync -avzP ~/Dotfiles/* ~/; \
-    fi
+if [ -d "~/Dotfiles" ]
+then 
+    rsync -avzP ~/Dotfiles/* ~/
+fi
 
 # vim-plug 
 
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -c PlugInstall
 sudo apt install nodejs
 
