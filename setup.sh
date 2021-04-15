342b0a804ef6d8e09ce9ba96dafa33b1d1f12cc3
Educorreia932 <skelozard1@gmail.com>
2021-04-15 15:02:12 +0100

    Now calling PlugInstall command silently in setup script

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ fi
 
 # vim-plug 
 
-# curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim -c PlugInstall
+curl -Lo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim -es -c PlugInstall
 sudo apt install nodejs
 
 # Git configuration
