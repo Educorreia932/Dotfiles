caf4b4423b16605f3f76ed96c357bb4e944bfe23
Educorreia932 <skelozard1@gmail.com>
2022-03-11 13:44:03 +0000

    Cleaned up setup.sh and added NeoVim installation from PPA

--- setup.sh
+++ setup.sh
@@ -14,26 +14,7 @@ git config --global user.name "Educorreia932"
 git config --global core.editor "nvim"
 
 # NeoVim 
-
-curl -Lo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sudo apt install nodejs
-nvim -es -c PlugInstall
-nvim -es -c CocInstall coc-pairs 
-
-# CCLS
-
-cd ~
-
-git clone --depth=1 --recursive https://github.com/MaskRay/ccls
-
-cd ccls
-
-sudo apt install clang cmake libclang-dev llvm-dev rapidjson-dev
-cmake -H. -BRelease
-cmake --build Release
-
-mv Release/ccls ~/.local/bin
-
-cd ~
-rm -rf ccls
-
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim
+ 
