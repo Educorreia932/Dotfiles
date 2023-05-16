1f6d9cf2ff63f5771c0268e79ddb83b5bf8ae7b0
Educorreia932 <skelozard1@gmail.com>
2023-05-17 02:09:50 +0900

    Removed comments from home.nix

--- home.nix
+++ home.nix
@@ -6,25 +6,8 @@
 
 	home.stateVersion = "22.11"; 
 
-	# The home.packages option allows you to install Nix packages into your
-	# environment.
 	home.packages = [
-		# # Adds the 'hello' command to your environment. It prints a friendly
-		# # "Hello, world!" when run.
-		# pkgs.hello
 
-		# # It is sometimes useful to fine-tune packages, for example, by applying
-		# # overrides. You can do that directly here, just don't forget the
-		# # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-		# # fonts?
-		# (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-		# # You can also create simple shell scripts directly inside your
-		# # configuration. For example, this adds a command 'my-hello' to your
-		# # environment:
-		# (pkgs.writeShellScriptBin "my-hello" ''
-		#   echo "Hello, ${config.home.username}!"
-		# '')
 	];
 
 	home.file = {
@@ -34,18 +17,7 @@
 		};
 	};
 
-	# You can also manage environment variables but you will have to manually
-	# source
-	#
-	#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-	#
-	# or
-	#
-	#  /etc/profiles/per-user/eduardo/etc/profile.d/hm-session-vars.sh
-	#
-	# if you don't want to manage your shell through Home Manager.
 	home.sessionVariables = {
-		# EDITOR = "emacs";
 	};
 
 	# Let Home Manager install and manage itself.
