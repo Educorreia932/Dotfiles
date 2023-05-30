c7555588ac11c91cb83c7b63ea6ed7b15b869168
Educorreia932 <skelozard1@gmail.com>
2023-05-30 22:19:44 +0900

    Added dconf settings

--- hosts/asus/home-manager.nix
+++ hosts/asus/home-manager.nix
@@ -6,4 +6,14 @@ in
 {
     home.username = "${user}";
     home.homeDirectory = "/home/${user}";
+
+    dconf.settings = {
+        "org/gnome/desktop/peripherals/touchpad" = {
+            "tap-to-click" = true;
+            "two-finger-scrolling-enabled" = true;
+        };
+        "org/gnome/desktop/interface" = {
+            "text-scaling-factor" = 1.25;
+        };
+    };
 } // import ../../common/home-manager.nix { config = config; pkgs = pkgs; lib = lib; }
