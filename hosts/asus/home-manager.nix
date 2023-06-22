2e0fa1be7479dfb1d7f7ba5bd588a61cd8c73c25
Educorreia932 <skelozard1@gmail.com>
2023-06-22 11:31:26 +0900

    Merging home-manager configurations

--- hosts/asus/home-manager.nix
+++ hosts/asus/home-manager.nix
@@ -1,5 +1,7 @@
 { config, pkgs, lib, ... }:
 
+with lib;
+
 let
     user = "eduardo";
 in
@@ -7,13 +9,40 @@ in
     home.username = "${user}";
     home.homeDirectory = "/home/${user}";
 
-    dconf.settings = {
-        "org/gnome/desktop/peripherals/touchpad" = {
-            "tap-to-click" = true;
-            "two-finger-scrolling-enabled" = true;
+    commonConfiguration = import ../../common/home-manager.nix { config = config; pkgs = pkgs; lib = lib; };
+
+    hostConfiguration = {
+        home.username = "${user}";
+        home.homeDirectory = "/home/${user}";
+
+        home.file =  {
+            i3 = {
+                source = ./config/i3;
+                target = ".config/i3";
+                recursive = true;
+            };
+	    };
+
+        dconf.settings = {
+            "org/gnome/desktop/peripherals/touchpad" = {
+                "tap-to-click" = true;
+                "two-finger-scrolling-enabled" = true;
+            };
+            "org/gnome/desktop/interface" = {
+                "text-scaling-factor" = 1.00;
+            };
         };
-        "org/gnome/desktop/interface" = {
-            "text-scaling-factor" = 1.25;
+
+        programs = {
+            pywal = {
+                enable = true;
+            };
+            vscode = {
+                enable = true;
+            };
         };
     };
-} // import ../../common/home-manager.nix { config = config; pkgs = pkgs; lib = lib; }
+
+    finalConfiguration = recursiveUpdate commonConfiguration hostConfiguration;
+in
+    finalConfiguration
