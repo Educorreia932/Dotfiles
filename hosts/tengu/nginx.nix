7cc470f98b83934a2e0cccd3b8310e06efbf9c71
Eduardo Correia <skelozard1@gmail.com>
2025-01-24 16:18:55 +0100

    👺 | Set up subdomains for hosts and SSL

--- hosts/tengu/nginx.nix
+++ hosts/tengu/nginx.nix
@@ -1,47 +1,47 @@
 { config, ... }:
 
+let
+  baseDomain = "educorreia932.dev";
+  mkVirtualHost = serverName: proxyAddress: port: {
+    listen = [
+      {
+        port = 80;
+        addr = "0.0.0.0";
+      }
+      {
+        port = 443;
+        addr = "0.0.0.0";
+        ssl = true;
+      }
+    ];
+    serverName = "${serverName}.${baseDomain}";
+    addSSL = true;
+    enableACME = true;
+    root = "/var/lib/www/${serverName}.${baseDomain}";
+    locations."/" = {
+      proxyPass = "http://localhost:${toString port}";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+      extraConfig = ''
+        client_max_body_size 50000M;
+        proxy_read_timeout   600s;
+        proxy_send_timeout   600s;
+        send_timeout         600s;
+      '';
+    };
+  };
+
+in
 {
   services.nginx = {
     enable = true;
     virtualHosts = {
-      # Immich
-      "immich" = {
-        listen = [
-          {
-            port = 8081;
-            addr = "0.0.0.0";
-          }
-        ];
-        locations."/" = {
-          proxyPass = "http://[::1]:${toString config.services.immich.port}";
-          proxyWebsockets = true;
-          recommendedProxySettings = true;
-          extraConfig = ''
-            client_max_body_size 50000M;
-            proxy_read_timeout   600s;
-            proxy_send_timeout   600s;
-            send_timeout         600s;
-          '';
-        };
-      };
-      # Paperless
-      "paperless" = {
-        listen = [
-          {
-            port = 8082;
-            addr = "0.0.0.0";
-          }
-        ];
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:${toString config.services.paperless.port}";
-          extraConfig = ''
-            client_max_body_size 50000M;
-            proxy_read_timeout   600s;
-            proxy_send_timeout   600s;
-            send_timeout         600s;
-          '';
-        };
-      };
+      "immich" = mkVirtualHost "immich" "[::1]" config.services.immich.port;
+      "paperless" = mkVirtualHost "paperless" "127.0.0.1" config.services.paperless.port;
     };
+  };
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "web+acme@company.org";
   };
 }
