be2c9455da24e147008a8d72a10cdabdee619b4f
Eduardo Correia <skelozard1@gmail.com>
2025-01-29 01:09:47 +0000

    👺 | Removed Paperless admin password

--- hosts/tengu/paperless.nix
+++ hosts/tengu/paperless.nix
@@ -2,9 +2,7 @@
   services.paperless = {
     enable = true;
     port = 28981;
-    passwordFile = "/etc/paperless-admin-pass";
     consumptionDirIsPublic = true;
     dataDir = "/mnt/media/paperless"; 
   };
-  environment.etc."paperless-admin-pass".text = "admin";
 }
