6fdcf8617bbe0d2c91b18f7a879a28f31a61840f
Eduardo Correia <skelozard1@gmail.com>
2025-01-30 12:05:52 +0000

    Updated zsh config

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -1,9 +1,16 @@
-{ user, ... }:
+{ user, pkgs, ... }:
 
 {
+  users.users.${user}.shell = pkgs.zsh;
+  programs.zsh.enable = true;
+
   home-manager.users.${user} = {
+    home.packages = with pkgs; [
+      fzf
+    ];
     programs.zsh = {
       enable = true;
+      autosuggestion.enable = true;
       oh-my-zsh = {
         enable = true;
         theme = "robbyrussell";
@@ -11,6 +18,7 @@
           "1password"
           "git"
           "sudo"
+          "fzf"
         ];
       };
       initExtra = builtins.readFile ./zshrc;
